b005f94c35363582e697a991d1610368973161da
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-10-26 05:28:50 -0400

    created JSON writer and unit test, cleaned up main - done adding functions

--- outputter.py
+++ outputter.py
@@ -2,17 +2,47 @@
 """
 
 import numpy as np
+import json
+import logging
+import analyzer
 
 
-def createDictionary(dictVals):
+def createDictionary(data):
     """Creates dictionary fitting desired template from values of interest.
 
-    :param dictVals: Python list containing 5 values of interest
+    :param data: numpy array of time and voltage data
     :returns: dictionary with values of interest assigned to appropriate keys
     """
-    metrics = {"mean_hr_bpm": dictVals[0],
-                "voltage_extremes": dictVals[1],
-                "duration": dictVals[2],
-                "num_beats": dictVals[3],
-                "beats": dictVals[4]}
+    duration = analyzer.produceDuration(data)
+    logging.info('Duration (s): {}'.format(duration))
+    voltageExtremes = analyzer.produceVoltageExtremes(data)
+    logging.info('Voltage Extremes (mv): {}'.format(voltageExtremes))
+    beatInds = analyzer.produceBeats(data)
+    numBeats = analyzer.produceNumBeats(beatInds)
+    logging.info('Number of Beats: {}'.format(numBeats))
+    timesOfBeats = analyzer.produceTimesOfBeats(data, beatInds)
+    logging.info('Times of Beats (s): {}'.format(timesOfBeats))
+    meanHR = analyzer.produceMeanHR(numBeats, duration)
+    logging.info('Mean Heart Rate (bpm): {}'.format(meanHR))
+    metrics = {"mean_hr_bpm": meanHR,
+                "voltage_extremes": voltageExtremes,
+                "duration": duration,
+                "num_beats": numBeats,
+                "beats": timesOfBeats}
+    logging.info("Metrics dictionary created.")
     return metrics
+
+
+def createJSON(metrics, inputfilename):
+    """Creates JSON file with metrics dictionary.
+
+    :param metrics: dictionary containing values of interest
+    :param inputfilename: original CSV filename input to program
+    :returns: name of JSON file
+    """
+    outputfilename = inputfilename[:-3] + "json"
+    metrics["beats"] = metrics["beats"].tolist()
+    with open(outputfilename, 'w') as JSONfile:
+        json.dump(metrics, JSONfile)
+    logging.info('Wrote JSON file: {}'.format(outputfilename))
+    return outputfilename
--- test_outputter.py
+++ test_outputter.py
@@ -3,6 +3,8 @@
 
 import pytest
 import numpy as np
+import json
+import logging
 
 
 def test_createDictionary(testData):
@@ -11,20 +13,7 @@ def test_createDictionary(testData):
     :returns: passes if dictionary was created as expected, fails otherwise
     """
     from outputter import createDictionary
-    from analyzer import produceDuration
-    from analyzer import produceVoltageExtremes
-    from analyzer import produceBeats
-    from analyzer import produceNumBeats
-    from analyzer import produceTimesOfBeats
-    from analyzer import produceMeanHR
-    duration = produceDuration(testData)
-    voltageExtremes = produceVoltageExtremes(testData)
-    beatInds = produceBeats(testData)
-    numBeats = produceNumBeats(beatInds)
-    timesOfBeats = produceTimesOfBeats(testData, beatInds)
-    meanHR = produceMeanHR(numBeats, duration)
-    dictVals = [meanHR, voltageExtremes, duration, numBeats, timesOfBeats]
-    metrics = createDictionary(dictVals)
+    metrics = createDictionary(testData)
     check = True
     if metrics.get("mean_hr_bpm") != 12:
         check = False
@@ -35,5 +24,32 @@ def test_createDictionary(testData):
     if metrics.get("num_beats") != 2:
         check = False
     if np.array_equal(metrics.get("beats"), np.array([3.5, 7.5])) is not True:
+        check = False
+    assert check is True
+
+
+def test_createJSON(testData, mktestfile):
+    """Tests createJSON function from outputter.py
+
+    :returns: passes if JSON was correctly created, fails otherwise
+    """
+    from outputter import createJSON
+    from outputter import createDictionary
+    trueDict = createDictionary(testData)
+    JSONfile = createJSON(trueDict, mktestfile)
+    with open(JSONfile, 'r') as readJSON:
+        insideDict = json.load(readJSON)
+    check = True
+    if insideDict.get("mean_hr_bpm") != 12:
+        check = False
+    if insideDict.get("voltage_extremes") != [0.0, 100.0]:
+        check = False
+        logging.warning('{}'.format(insideDict.get("voltage_extremes")))
+        logging.warning('{}'.format(trueDict.get("voltage_extremes")))
+    if insideDict.get("duration") != 10:
+        check = False
+    if insideDict.get("num_beats") != 2:
+        check = False
+    if insideDict.get("beats") != [3.5, 7.5]:
         check = False
     assert check is True
